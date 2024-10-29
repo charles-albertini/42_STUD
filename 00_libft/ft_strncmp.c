1ee14e2d74a657b97fe7824cdec97f9a219ed9c9
charles albertini <charles.albertinirosso@gmail.com>
2024-10-29 21:17:29 +0100

    strncmp

--- 00_libft/ft_strncmp.c
+++ 00_libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calberti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: calberti <calberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:37:49 by calberti          #+#    #+#             */
-/*   Updated: 2024/10/14 12:37:51 by calberti         ###   ########.fr       */
+/*   Updated: 2024/10/29 21:16:34 by calberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	nbr;
+	unsigned char	*s3;
+	unsigned char	*s4;
 
+	s3 = (unsigned char *)s1;
+	s4 = (unsigned char *)s2;
 	nbr = 0;
-	while ((s1[nbr] || s2[nbr]) && nbr < n)
+	while ((s3[nbr] || s4[nbr]) && nbr < n)
 	{
-		if (s1[nbr] < s2[nbr])
-			return (s1[nbr] - s2[nbr]);
-		if (s1[nbr] > s2[nbr])
-			return (s1[nbr] - s2[nbr]);
+		if (s3[nbr] < s4[nbr])
+			return (s3[nbr] - s4[nbr]);
+		if (s3[nbr] > s4[nbr])
+			return (s3[nbr] - s4[nbr]);
 		nbr ++;
 	}
 	return (0);
@@ -33,10 +37,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 int main (int argc, char **argv)
 {
-	size_t n = 5;
+	size_t n = 6;
 	if (argc == 3)
 	{
-	printf("%d\n", ft_strncmp(argv[1], argv[2], n));
+	printf("%d\n", ft_strncmp("test\200", "test\0", n));
 	printf("%d\n", strncmp(argv[1], argv[2], n));
 
 	}
