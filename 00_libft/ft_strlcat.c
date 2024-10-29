56bcb5021643df0bd830b65f632a2255e30e68a0
charles albertini <charles.albertinirosso@gmail.com>
2024-10-29 20:49:28 +0100

    strlcat

--- 00_libft/ft_strlcat.c
+++ 00_libft/ft_strlcat.c
@@ -3,55 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calberti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: calberti <calberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:37:10 by calberti          #+#    #+#             */
-/*   Updated: 2024/10/14 12:37:12 by calberti         ###   ########.fr       */
+/*   Updated: 2024/10/29 20:46:55 by calberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	nbr;
-	size_t	size_dest;
+	size_t	i;
+	size_t	len_dest;
+	size_t	len_src;
 
-	nbr = 0;
-	size_dest = ft_strlen(src);
-	if (size && dest != src)
+	i = 0;
+	len_dest = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size <= len_dest) // si on a pas la place de copier 
+		return (size + len_src);
+	while (src[i] != '\0' && i < (size - len_dest - 1))
 	{
-		if (size_dest >= size)
-		{
-			return (size_dest + size);
-		}
-		while (src[nbr] && nbr < (size - 1))
-		{
-			dest [size_dest + nbr] = src [nbr];
-			nbr ++;
-		}
-		dest [size_dest + nbr] = 0;
+		dst[len_dest + i] = src[i];
+		i++;
 	}
-	while (src[nbr])
-	{
-		nbr ++;
-	}
-	return (nbr + size_dest);
+	dst[len_dest + i] = '\0';
+	return (len_dest + len_src);
 }
+
+
+
 /*
 #include <string.h>
 #include <stdio.h>
  
 int main (void)
 {
-char  *test = "salut";
-char *test2 = "bommmmlkh";
-unsigned int i = 3;
+char  dest[10] = "salut";
+const char *src = "comment";
+unsigned int i = 0;
  
-printf("%zu \n", ft_strlcat(test, test2, i));
-printf("%zu \n", strlcat(test, test2, i));
-//printf("%s \n", test);
+printf("%zu \n", ft_strlcat(dest, src, i));
+printf("%s \n", dest);
 //n = size de dest - len de dest - 1 = 12 - 5 - 1 = 6
  
 }
 */
+
